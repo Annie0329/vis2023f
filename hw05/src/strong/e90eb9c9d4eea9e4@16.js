46ad54336aacf905957c09c16e2a61241149c758
Annie0329 <0329annie@gmail.com>
2023-12-01 16:56:29 +0800

    1201

--- hw05/src/strong/e90eb9c9d4eea9e4@16.js
+++ hw05/src/strong/e90eb9c9d4eea9e4@16.js
@@ -240,7 +240,7 @@ export default function define(runtime, observer) {
   const main = runtime.module();
   function toString() { return this.url; }
   const fileAttachments = new Map([
-    ["output@1.json", {url: new URL("../output.json", import.meta.url), mimeType: "application/json", toString}]
+    ["output@1.json", {url: new URL("../outputSimple.json", import.meta.url), mimeType: "application/json", toString}]
   ]);
   main.builtin("FileAttachment", runtime.fileAttachments(name => fileAttachments.get(name)));
   main.variable(observer()).define(["md"], _1);
